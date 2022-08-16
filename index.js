be2b9632b63b66b7de959ae510fb225eb8f1071d
Alpha Bravo <bautista.albert@gmail.com>
2022-08-16 00:31:50 -0500

    Update index.js and package.json

---
+++ index.js
@@ -0,0 +1,5 @@
+const inquirer = require('inquirer');
+const employee = require('./lib/employee.js');
+const engineer = require('./lib/engineer');
+const intern = require('./lib/intern.js');
+const manager = require('./lib/manager.js');
